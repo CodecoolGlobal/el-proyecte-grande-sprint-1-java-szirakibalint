7912ce790e4aaf38a574097ed9a30b04964673b7
Balint Kozari <mrkoba@gmail.com>
2021-11-30 17:57:47 +0100

    feat: create API endpoint for buy and sell

--- src/main/java/com/codecool/codecoin/controller/APIController.java
+++ src/main/java/com/codecool/codecoin/controller/APIController.java
@@ -3,10 +3,7 @@ package com.codecool.codecoin.controller;
 import com.codecool.codecoin.dao.CryptocurrencyDAO;
 import com.codecool.codecoin.model.Cryptocurrency;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Set;
 
@@ -28,5 +25,15 @@ public class APIController {
     @GetMapping("/coins/{id}")
     public Cryptocurrency getCurrencyById(@PathVariable String id) {
         return cryptocurrencyDAO.getCurrencyById(id);
+    }
+
+    @PostMapping("/coins/{id}")
+    public String buyCurrency(@PathVariable String id, @RequestParam int amount) {
+        return "Bought " + String.valueOf(amount) + " of " + id;
+    }
+
+    @PutMapping("/coins/{id}")
+    public String sellCurrency(@PathVariable String id, @RequestParam int amount) {
+        return "Sold " + String.valueOf(amount) + " of " + id;
     }
 }
