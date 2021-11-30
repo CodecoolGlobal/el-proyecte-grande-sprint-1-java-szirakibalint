69b39a3eada018fedca87d7c86b19943bc73a40b
Balint Kozari <mrkoba@gmail.com>
2021-11-30 09:49:07 +0100

    feat: create starter project

---
+++ src/main/java/com/codecool/codecoin/CodeCoinApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.codecoin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeCoinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeCoinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codecool/codecoin/CodecoinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.codecoin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodecoinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
