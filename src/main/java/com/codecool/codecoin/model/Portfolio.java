c475b293d6d4d5f4fb6b1b886aaa3b288f491b7c
Balint Kozari <mrkoba@gmail.com>
2021-11-30 20:17:44 +0100

    feat: create portfolio class to store currencies #1

--- src/main/java/com/codecool/codecoin/model/Portfolio.java
+++ src/main/java/com/codecool/codecoin/model/Portfolio.java
@@ -1,2 +1,39 @@
-package com.codecool.codecoin.model;public class Portfolio {
+package com.codecool.codecoin.model;
+
+import java.util.Currency;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Portfolio {
+
+    private Map<Cryptocurrency, Integer> currencies;
+    private static Portfolio instance;
+
+    private Portfolio() {
+        currencies = new HashMap<>();
+    }
+
+    public static Portfolio getInstance() {
+        if (instance == null) {
+            instance = new Portfolio();
+        }
+        return instance;
+    }
+
+    public boolean buyCrypto(Cryptocurrency currency, int amount) {
+        if (!currencies.containsKey(currency)) {
+            currencies.put(currency, amount);
+        } else {
+            currencies.put(currency, currencies.get(currency) + amount);
+        }
+        return true;
+    }
+
+    public boolean sellCrypto(Cryptocurrency currency, int amount) {
+        if (currencies.containsKey(currency) && currencies.get(currency) >= amount) {
+            currencies.put(currency, currencies.get(currency) - amount);
+            return true;
+        }
+        return false;
+    }
 }
