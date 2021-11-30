af975a83fedf9e8d22ddd3fef8bd0f3a50d73bed
Balint Kozari <mrkoba@gmail.com>
2021-11-30 15:56:19 +0100

    feat: implement getCurrencyByID method in APIController #9

--- src/main/java/com/codecool/codecoin/controller/APIController.java
+++ src/main/java/com/codecool/codecoin/controller/APIController.java
@@ -4,6 +4,7 @@ import com.codecool.codecoin.dao.CryptocurrencyDAO;
 import com.codecool.codecoin.model.Cryptocurrency;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,7 +13,7 @@ import java.util.Set;
 @RestController
 @RequestMapping("/api")
 public class APIController {
-    CryptocurrencyDAO cryptocurrencyDAO;
+    private CryptocurrencyDAO cryptocurrencyDAO;
 
     @Autowired
     public APIController(CryptocurrencyDAO cryptocurrencyDAO) {
@@ -22,5 +23,10 @@ public class APIController {
     @GetMapping("/coins")
     public Set<Cryptocurrency> getAll() {
         return cryptocurrencyDAO.getAll();
+    }
+
+    @GetMapping("/coins/{id}")
+    public Cryptocurrency getCurrencyById(@PathVariable String id) {
+        return cryptocurrencyDAO.getCurrencyById(id);
     }
 }
