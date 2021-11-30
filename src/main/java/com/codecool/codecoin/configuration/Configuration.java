0671bcb212f16197c74309594e5183b5f7e14b02
szirakibalint <blntsziraki@gmail.com>
2021-11-30 14:49:42 +0100

    feat: add Configuration class #1

---
+++ src/main/java/com/codecool/codecoin/configuration/Configuration.java
@@ -0,0 +1,12 @@
+package com.codecool.codecoin.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@org.springframework.context.annotation.Configuration
+public class Configuration {
+    @Bean
+    public WebClient getWebClient() {
+        return WebClient.create();
+    }
+}
