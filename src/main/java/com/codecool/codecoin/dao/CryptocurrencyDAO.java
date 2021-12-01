332a6ee4cfa1db9c6f4099e955c20ecf420ff49f
szirakibalint <blntsziraki@gmail.com>
2021-12-01 12:20:39 +0100

    docs: describe CryptocurrencyDAO interface #2

--- src/main/java/com/codecool/codecoin/dao/CryptocurrencyDAO.java
+++ src/main/java/com/codecool/codecoin/dao/CryptocurrencyDAO.java
@@ -4,6 +4,9 @@ import com.codecool.codecoin.model.Cryptocurrency;
 
 import java.util.Set;
 
+/**
+ * Contains method signatures for basic Cryptocurrency data access operations.
+ */
 public interface CryptocurrencyDAO {
     Set<Cryptocurrency> getAll();
     Cryptocurrency getCurrencyById(String id);
