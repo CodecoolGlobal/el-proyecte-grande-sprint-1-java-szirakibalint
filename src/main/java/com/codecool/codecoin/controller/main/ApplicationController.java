26382da052de1ff18ddfe51fa175c24ed61aa405
barramartin <78955308+barramartin@users.noreply.github.com>
2021-12-01 11:53:29 +0100

    feat: implement /coins route

--- src/main/java/com/codecool/codecoin/controller/main/ApplicationController.java
+++ src/main/java/com/codecool/codecoin/controller/main/ApplicationController.java
@@ -1,13 +1,29 @@
 package com.codecool.codecoin.controller.main;
 
+import com.codecool.codecoin.controller.APIController;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ApplicationController {
 
+    private APIController apiController;
+
+    @Autowired
+    public ApplicationController(APIController apiController) {
+        this.apiController = apiController;
+    }
+
     @GetMapping("/")
     public String index() {
         return "index";
+    }
+
+    @GetMapping("/coins")
+    public String coins(Model model) {
+        model.addAttribute("allCurrencies", apiController.getAll());
+        return "coins";
     }
 }
