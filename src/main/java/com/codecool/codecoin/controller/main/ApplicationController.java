925e7f2536877ea76d7bd14b38f31a0c002ab3d5
barramartin <78955308+barramartin@users.noreply.github.com>
2021-12-01 14:26:19 +0100

    feat: implement route for portfolio page #6

--- src/main/java/com/codecool/codecoin/controller/main/ApplicationController.java
+++ src/main/java/com/codecool/codecoin/controller/main/ApplicationController.java
@@ -2,11 +2,17 @@ package com.codecool.codecoin.controller.main;
 
 import com.codecool.codecoin.controller.APIController;
 import com.codecool.codecoin.model.Cryptocurrency;
+import com.codecool.codecoin.model.CurrencyType;
+import com.codecool.codecoin.model.Portfolio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+
+import java.math.BigDecimal;
+import java.util.Currency;
+import java.util.Map;
 
 @Controller
 public class ApplicationController {
@@ -38,5 +44,21 @@ public class ApplicationController {
             model.addAttribute("cryptocurrency", cryptocurrency);
             return "coin-details";
         }
+    }
+
+    @GetMapping("/portfolio")
+    public String portfolio(Model model) {
+        Portfolio portfolio = apiController.getPortfolio();
+        Map<CurrencyType, BigDecimal> currencies = portfolio.getCurrencies();
+        Map<Cryptocurrency, BigDecimal> cryptocurrencies = portfolio.getCryptoCurrencies();
+        BigDecimal totalBalance = currencies.get(CurrencyType.USD);
+        for (Map.Entry<Cryptocurrency, BigDecimal> entry : cryptocurrencies.entrySet()) {
+            BigDecimal value = entry.getKey().getCurrentPrice().multiply(entry.getValue());
+            totalBalance = totalBalance.add(value);
+        }
+        model.addAttribute("cryptocurrencies", cryptocurrencies);
+        model.addAttribute("currencies", currencies);
+        model.addAttribute("totalBalance", totalBalance);
+        return "portfolio";
     }
 }
