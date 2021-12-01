ad465b9a1d3695b762b4b8b62f426c53ace9db3c
barramartin <78955308+barramartin@users.noreply.github.com>
2021-12-01 12:56:08 +0100

    feat: implement route for detailed coin view #5

--- src/main/java/com/codecool/codecoin/controller/main/ApplicationController.java
+++ src/main/java/com/codecool/codecoin/controller/main/ApplicationController.java
@@ -1,10 +1,12 @@
 package com.codecool.codecoin.controller.main;
 
 import com.codecool.codecoin.controller.APIController;
+import com.codecool.codecoin.model.Cryptocurrency;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class ApplicationController {
@@ -25,5 +27,16 @@ public class ApplicationController {
     public String coins(Model model) {
         model.addAttribute("allCurrencies", apiController.getAll());
         return "coins";
+    }
+
+    @GetMapping("/coins/{coinId}")
+    public String coinDetails(@PathVariable String coinId, Model model) {
+        Cryptocurrency cryptocurrency = apiController.getCurrencyById(coinId);
+        if (cryptocurrency == null) {
+            return "error";
+        } else {
+            model.addAttribute("cryptocurrency", cryptocurrency);
+            return "coin-details";
+        }
     }
 }
