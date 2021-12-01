0a91cb849964ff02f108a4467a7364021de815a4
szirakibalint <blntsziraki@gmail.com>
2021-12-01 14:38:03 +0100

    docs: describe ApplicationController class #2

--- src/main/java/com/codecool/codecoin/controller/main/ApplicationController.java
+++ src/main/java/com/codecool/codecoin/controller/main/ApplicationController.java
@@ -8,6 +8,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+/**
+ * Contains methods for handling page display for routes.
+ */
 @Controller
 public class ApplicationController {
 
