b235806f4677bf71942b857cacdf6ba2c22243ac
Balint Kozari <mrkoba@gmail.com>
2021-12-02 13:39:43 +0100

    fix: update calculator to use realtime data #6

--- src/main/java/com/codecool/codecoin/logic/Calculator.java
+++ src/main/java/com/codecool/codecoin/logic/Calculator.java
@@ -1,20 +1,23 @@
 package com.codecool.codecoin.logic;
 
+import com.codecool.codecoin.controller.APIController;
 import com.codecool.codecoin.model.Cryptocurrency;
 import com.codecool.codecoin.model.CurrencyType;
 import com.codecool.codecoin.model.Portfolio;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.math.BigDecimal;
 import java.util.Map;
 
 public class Calculator {
 
-    public BigDecimal calculateTotalBalance(Portfolio portfolio) {
+    public BigDecimal calculateTotalBalance(Portfolio portfolio, APIController apiController) {
         Map<CurrencyType, BigDecimal> currencies = portfolio.getCurrencies();
         Map<Cryptocurrency, BigDecimal> cryptocurrencies = portfolio.getCryptoCurrencies();
         BigDecimal totalBalance = currencies.get(CurrencyType.USD);
         for (Map.Entry<Cryptocurrency, BigDecimal> entry : cryptocurrencies.entrySet()) {
-            BigDecimal value = entry.getKey().getCurrentPrice().multiply(entry.getValue());
+            Cryptocurrency cryptocurrencyActualData = apiController.getCurrencyById(entry.getKey().getId());
+            BigDecimal value = cryptocurrencyActualData.getCurrentPrice().multiply(entry.getValue());
             totalBalance = totalBalance.add(value);
         }
         return totalBalance;
