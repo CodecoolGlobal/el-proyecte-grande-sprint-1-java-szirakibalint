42ce02e89798028293849cf1f3e80bb8792231f5
Balint Kozari <mrkoba@gmail.com>
2021-12-13 16:44:42 +0100

    feat[#38]: implement getPortfolio method in userDaoMemory

--- src/main/java/com/codecool/codecoin/dao/implementation/UserDAOMemory.java
+++ src/main/java/com/codecool/codecoin/dao/implementation/UserDAOMemory.java
@@ -1,2 +1,20 @@
-package com.codecool.codecoin.dao.implementation;public class UserDAOMemory {
+package com.codecool.codecoin.dao.implementation;
+
+import com.codecool.codecoin.dao.UserDAO;
+import com.codecool.codecoin.model.Portfolio;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class UserDAOMemory implements UserDAO {
+
+    private final Portfolio portfolio;
+
+    @Autowired
+    public UserDAOMemory(Portfolio portfolio) {
+        this.portfolio = portfolio;
+    }
+
+    @Override
+    public Portfolio getPortfolio() {
+        return portfolio;
+    }
 }
