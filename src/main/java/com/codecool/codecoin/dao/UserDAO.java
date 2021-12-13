d47aadafa1823c61b4b0a69ad5591fd2f71e64a2
Balint Kozari <mrkoba@gmail.com>
2021-12-13 17:03:56 +0100

    feat[#38]: add buy and sell methods to userDao

--- src/main/java/com/codecool/codecoin/dao/UserDAO.java
+++ src/main/java/com/codecool/codecoin/dao/UserDAO.java
@@ -2,7 +2,11 @@ package com.codecool.codecoin.dao;
 
 import com.codecool.codecoin.model.Portfolio;
 
+import java.math.BigDecimal;
+
 public interface UserDAO {
 
     Portfolio getPortfolio();
+    String buyCrypto(String id, BigDecimal amount);
+    String sellCrypto(String id, BigDecimal amount);
 }
