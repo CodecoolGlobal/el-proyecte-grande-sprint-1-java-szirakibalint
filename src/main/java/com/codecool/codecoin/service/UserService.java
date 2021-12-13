5ab91b8e24852284d47ca247e67662b442848014
Balint Kozari <mrkoba@gmail.com>
2021-12-13 17:09:07 +0100

    feat[#38]: implement portfolio, buy and sell methods in user service

--- src/main/java/com/codecool/codecoin/service/UserService.java
+++ src/main/java/com/codecool/codecoin/service/UserService.java
@@ -1,2 +1,31 @@
-package com.codecool.codecoin.service;public class UserService {
+package com.codecool.codecoin.service;
+
+import com.codecool.codecoin.dao.UserDAO;
+import com.codecool.codecoin.model.Portfolio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+@Service
+public class UserService {
+
+    private final UserDAO userDAO;
+
+    @Autowired
+    public UserService(UserDAO userDAO) {
+        this.userDAO = userDAO;
+    }
+
+    public Portfolio getPortfolio() {
+        return userDAO.getPortfolio();
+    }
+
+    public String buyCryptoCurrency(String id, BigDecimal amount) {
+        return userDAO.buyCrypto(id, amount);
+    }
+
+    public String sellCryptocurrency(String id, BigDecimal amount) {
+        return userDAO.sellCrypto(id, amount);
+    }
 }
