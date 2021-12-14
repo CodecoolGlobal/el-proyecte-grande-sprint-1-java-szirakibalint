f63f56e6b25009351da495a62d7b1316a873fa09
Balint Kozari <mrkoba@gmail.com>
2021-12-14 20:26:26 +0100

    feat[#34]: update details component so it can fetch from codecoin api

--- client/codecoin/src/routes/Details.js
+++ client/codecoin/src/routes/Details.js
@@ -10,22 +10,19 @@ function Details() {
             setCoin(coinFromAPI)
         }
         getCoin();
-        console.log(coin.name)
     }, [])
 
     const fetchCoin = async () => {
-        const res = await fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=${id}`)
+        const res = await fetch(`/api/coins/${id}`)
         return await res.json()
     }
     return (
         <>
-            <div>{coin.map((coin) => (
-                <div>
+            <div>
                     <p>{coin.name}</p>
                     <p>{coin.current_price}</p>
                     <p>{coin.high_24h}</p>
                     <p>{coin.low_24h}</p>
-                </div>))}
             </div>
         </>
     )
