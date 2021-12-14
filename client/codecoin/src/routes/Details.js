f46245ac4e1b6e3d55b3e65d911e476e877faa0b
Balint Kozari <mrkoba@gmail.com>
2021-12-14 21:30:53 +0100

    refactor[#34]: remove duplicated code from details

--- client/codecoin/src/routes/Details.js
+++ client/codecoin/src/routes/Details.js
@@ -1,21 +1,9 @@
-import {useEffect, useState} from "react";
 import {useParams} from "react-router-dom";
+import CoinFetcher from "../components/CoinFetcher";
 
 function Details() {
-    const [coin, setCoin] = useState([])
     let { id } = useParams();
-    useEffect(() => {
-        const getCoin = async () => {
-            const coinFromAPI = await fetchCoin()
-            setCoin(coinFromAPI)
-        }
-        getCoin();
-    }, [])
-
-    const fetchCoin = async () => {
-        const res = await fetch(`/api/coins/${id}`)
-        return await res.json()
-    }
+    const coin = CoinFetcher(id);
     return (
         <>
             <div>
