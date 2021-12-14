e40f3b03248cd8c7888f68361f64576ac6dc05da
barramartin <78955308+barramartin@users.noreply.github.com>
2021-12-14 16:12:27 +0100

    feat[#34]: LandingPage.js is filled with content

--- client/codecoin/src/routes/LandingPage.js
+++ client/codecoin/src/routes/LandingPage.js
@@ -3,6 +3,18 @@ import {Link} from "react-router-dom";
 function LandingPage() {
     return (
         <div>
+            <div className="title">
+                <p className="text primary">We are </p>
+                <p className="text secondary" id="text">the dovari</p>
+            </div>
+            <div className="buttons">
+                <Link to="/coins">
+                    <button className="button btn-primary">See currencies</button>
+                </Link>
+                <Link to="/portfolio">
+                    <button className="button btn-secondary">Get started</button>
+                </Link>
+            </div>
         </div>
     )
 }
