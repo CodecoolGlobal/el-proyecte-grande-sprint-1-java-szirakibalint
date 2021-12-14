3729604f975fc8a34cc7f17c775447adaa43b225
Balint Kozari <mrkoba@gmail.com>
2021-12-14 21:30:28 +0100

    refactor[#34]: remove duplicated code from coins

--- client/codecoin/src/routes/Coins.js
+++ client/codecoin/src/routes/Coins.js
@@ -1,19 +1,9 @@
 import {useEffect, useState} from "react";
+import {useParams} from "react-router-dom";
+import CoinFetcher from "../components/CoinFetcher";
 
 function Coins() {
-    const [coins, setCoins] = useState([])
-    useEffect(() => {
-        const getCoins = async () => {
-            const coinsFromAPI = await fetchCoins()
-            setCoins(coinsFromAPI)
-        }
-        getCoins();
-    }, [])
-
-    const fetchCoins = async () => {
-        const res = await fetch(`/api/coins`)
-        return await res.json()
-    }
+    const coins = CoinFetcher('');
     return (
         <>
             <div>{coins.map((coin) => (
