7709470809105269aedb77dd491e1d9446e698a4
barramartin <78955308+barramartin@users.noreply.github.com>
2021-12-14 16:14:22 +0100

    feat[#34]: add fetch to Coin component

--- client/codecoin/src/routes/Coins.js
+++ client/codecoin/src/routes/Coins.js
@@ -2,6 +2,19 @@ import DataHandler from "../components/DataHandler";
 import {useEffect, useState} from "react";
 
 function Coins() {
+    const [coins, setCoins] = useState([])
+    useEffect(() => {
+        const getCoins = async () => {
+            const coinsFromAPI = await fetchCoins()
+            setCoins(coinsFromAPI)
+        }
+        getCoins();
+    }, [])
+
+    const fetchCoins = async () => {
+        const res = await fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd`)
+        return await res.json()
+    }
 }
 
 export default Coins;
