eead1144d09df99cdaa1ee3941f4a22c0b39f70f
Balint Kozari <mrkoba@gmail.com>
2021-12-15 16:57:00 +0100

    feat[#34]: return to error page when fetching failes

--- client/codecoin/src/routes/Coins.js
+++ client/codecoin/src/routes/Coins.js
@@ -1,9 +1,13 @@
 import CoinFetcher from "../components/CoinFetcher";
 import Card from "../components/Card";
 import CoinsModule from "../styles/Coins.css"
+import Error from "./Error";
 
 function Coins() {
     const coins = CoinFetcher('');
+    if (coins.length === 0) {
+        return Error();
+    }
     return (
         <>
             <dic className="header"><h1>Currencies</h1></dic>
