c16d9cbaf953cb5a216026606f69639926e3244c
barramartin <78955308+barramartin@users.noreply.github.com>
2021-12-15 14:28:01 +0100

    feat[#34]: add Card.js component

---
+++ client/codecoin/src/components/Card.js
@@ -0,0 +1,11 @@
+import CardModule from "../styles/Card.css"
+import {Link} from "react-router-dom";
+
+function Card(props) {
+    return (
+        <div className={"currency"}>
+        </div>
+    )
+}
+
+export default Card;
