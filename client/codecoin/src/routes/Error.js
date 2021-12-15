331ddca8aafad4bcc1abb7ac0dbd6c40efb6772c
Balint Kozari <mrkoba@gmail.com>
2021-12-15 18:27:23 +0100

    feat[#34]: add customized error message

--- client/codecoin/src/routes/Error.js
+++ client/codecoin/src/routes/Error.js
@@ -1,10 +1,11 @@
 import {Link} from "react-router-dom";
 
-function Error() {
+function Error(message) {
+    message = message === undefined ? "Ooops something went wrong." : message;
     return (
         <>
         <img src="https://i.postimg.cc/k5zT5dmN/logo-broken.png" width="800px" alt="logo"/>
-            <h1 id="error-message">Ooops something went wrong.</h1>
+            <h1 id="error-message">{message}</h1>
             <h1>You can report the issue or simply go back to the main page.</h1>
             <Link to="/coins">
                 <button className="button">Report the issue!</button>
