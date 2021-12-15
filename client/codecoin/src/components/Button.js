6f4947ec6790e4b9d740ff64318df9f57f644578
barramartin <78955308+barramartin@users.noreply.github.com>
2021-12-15 14:25:01 +0100

    feat[#34]: Button.js returns proper button tag

--- client/codecoin/src/components/Button.js
+++ client/codecoin/src/components/Button.js
@@ -4,7 +4,7 @@ import ButtonModule from "../styles/Button.css"
 function Button(props) {
     return (
         <div>
-
+            <button className={props.className}>{props.text}</button>
         </div>
     )
 }
