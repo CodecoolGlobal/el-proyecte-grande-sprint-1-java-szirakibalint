6076c409f69124e9c98f543396d6ee30bc986746
Balint Kozari <mrkoba@gmail.com>
2021-12-15 23:47:09 +0100

    feat[#34]: update buy to use preview offer

--- client/codecoin/src/routes/Buy.js
+++ client/codecoin/src/routes/Buy.js
@@ -1,23 +1,13 @@
 import {useParams} from "react-router-dom";
 import CoinFetcher from "../components/CoinFetcher"
 import PortfolioFetcher from "../components/PortfolioFetcher";
+import PreviewOffer from "../components/PreviewOffer";
 
 function Buy() {
 
     let { id } = useParams();
     const coin = CoinFetcher(id);
     const portfolio = PortfolioFetcher();
-    async function handleBuy() {
-        const inputField = document.querySelector('#amount-input');
-        await fetch(`/api/coins/${id}`, {
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            method: 'POST',
-            body: JSON.stringify({"amount": parseFloat(inputField.value)})
-        })
-    }
     return (
             <>
                 <div className="header">
@@ -29,13 +19,7 @@ function Buy() {
                     <h3>{coin.name} balance: {portfolio.portfolio !== undefined && portfolio.portfolio.cryptoCurrencies !== undefined && portfolio.portfolio.cryptoCurrencies[coin.id] || 0} {coin.symbol}</h3>
                     <h3>1 {coin.symbol} = {coin.current_price} USD</h3>
                 </div>
-                <div className="transaction">
-                    <label htmlFor="amount-input">How much {coin.name} you want to buy?</label>
-                    <input id="amount-input" type="text" name="amount"/>
-                        <form className="transaction-form">
-                            <button onClick={handleBuy} id="buy-button" className="button" type="submit">Buy</button>
-                        </form>
-                </div>
+                <PreviewOffer coin={coin} buyOrSell="buy"/>
             </>
     )
 }
