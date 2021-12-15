11c4365a36eb10bbf262329b67d362ce9b2e61f8
Balint Kozari <mrkoba@gmail.com>
2021-12-15 19:15:51 +0100

    feat[#34]: change return type from array to react element

--- client/codecoin/src/components/CoinFetcher.js
+++ client/codecoin/src/components/CoinFetcher.js
@@ -1,4 +1,5 @@
 import {useEffect, useState} from "react";
+import Error from "../routes/Error";
 
 function CoinFetcher(id) {
     const [coin, setCoin] = useState([])
@@ -21,9 +22,9 @@ function CoinFetcher(id) {
             return await res.json()
         } catch (e) {
             if (id === '') {
-                return [{"error": "Error: failed to fetch data from API endpoint /api/coins"}];
+                return Error(`Error: failed to fetch coin data from API endpoint /api/coins/`);
             } else {
-                return [{"error": `Error: failed to fetch coin data from API endpoint /api/coins/${id}`}];
+                return Error(`Error: failed to fetch coin data from API endpoint /api/coins/${id}`);
             }
         }
     }
