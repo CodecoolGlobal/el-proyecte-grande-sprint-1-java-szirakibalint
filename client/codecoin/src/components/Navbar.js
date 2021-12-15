85135e8faa1b57e38353748614a35c316d125df8
barramartin <78955308+barramartin@users.noreply.github.com>
2021-12-15 14:19:25 +0100

    feat[#34]: Navbar.js imports Navbar.css

--- client/codecoin/src/components/Navbar.js
+++ client/codecoin/src/components/Navbar.js
@@ -1,10 +1,11 @@
 import {Link} from "react-router-dom";
+import NavbarModule from "../styles/Navbar.css"
 
 function Navbar() {
     return (
         <div className="navbar">
             <div id="logo-container">
-                <Link to='/'><img src='https://i.postimg.cc/J46SZv3g/logo.png' width="200px" alt='logo'/></Link>
+                <Link to='/'><img src='https://i.postimg.cc/J46SZv3g/logo.png' alt='logo'/></Link>
             </div>
             <div id="nav-options">
                 <Link to="/coins">Currencies</Link>
