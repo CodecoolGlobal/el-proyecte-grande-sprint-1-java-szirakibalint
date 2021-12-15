4fe45e2ef26e8fb8868d8f3046402db7fd460bcf
szirakibalint <blntsziraki@gmail.com>
2021-12-15 17:08:49 +0100

    feat[#34]: fetch current price data in Portfolio.js

--- client/codecoin/src/routes/Portfolio.js
+++ client/codecoin/src/routes/Portfolio.js
@@ -1,9 +1,29 @@
 import PortfolioFetcher from "../components/PortfolioFetcher";
+import CoinFetcher from "../components/CoinFetcher";
 
 function Portfolio() {
     const fullPortfolio = PortfolioFetcher();
     const portfolio = fullPortfolio.portfolio;
     const totalBalance = fullPortfolio.totalBalance;
+
+    const coins = CoinFetcher('');
+
+    const portfolioHasCoins = (portfolio) && (portfolio.cryptoCurrencies);
+    const portfolioCoins = [];
+
+    if (portfolioHasCoins) {
+        const currencies = portfolio.cryptoCurrencies;
+        const portfolioCoinIds = Object.keys(currencies);
+        for (let coin of coins) {
+            if (portfolioCoinIds.includes(coin.id)) {
+                portfolioCoins.push(
+                    {...coin,
+                    "amount": currencies[coin.id]
+                    });
+            }
+        }
+    }
+
     return (
         <>
             <div className="header">
@@ -11,10 +31,12 @@ function Portfolio() {
                 <h1>{totalBalance} USD</h1>
             </div>
             <div className="portfolio-content">
-                {portfolio !== undefined && portfolio.cryptoCurrencies !== undefined && Object.keys(portfolio.cryptoCurrencies).map((key) => (
-                <div className="portfolio-card">
-                    <p>{key} : {portfolio.cryptoCurrencies[key]}</p>
-                </div>))}
+                {portfolioHasCoins ? portfolioCoins.map(coin => (
+                    <div className="portfolio-coin">
+                        <p>{coin.name}</p>
+                        <p>{coin.amount}</p>
+                        <p>{coin.current_price * coin.amount}</p>
+                    </div>)) : null}
             </div>
         </>
     )
