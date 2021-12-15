04d2649a1499151852dacb323b3667412eff93b9
barramartin <78955308+barramartin@users.noreply.github.com>
2021-12-15 14:40:53 +0100

    feat[#34]: Details.js displays the details about the currency

--- client/codecoin/src/routes/Details.js
+++ client/codecoin/src/routes/Details.js
@@ -13,7 +13,21 @@ function Details() {
                 <img src={coin.image} className={"detailed-img"} alt=""/>
                 <h1>{coin.name}</h1>
             </div>
-
+            <div className="action">
+                <Link to={'/coins/' + coin.id + '/buy'}><Button id={"buy"} text={"Buy"} class={"button btn-contained"}></Button></Link>
+                <Link to={'/coins/' + coin.id + '/sell'}><Button id={"sell"} text={"Sell"} class={"button btn-contained"}></Button></Link>
+            </div>
+            <div className="content">
+                <div className="details">
+                    <h3 id="details">Details</h3>
+                    <p className="tag">Name:</p>
+                    <p className="text">{coin.name}</p>
+                    <p className="tag">Symbol:</p>
+                    <p className="text">{coin.symbol}</p>
+                    <p className="tag">Last update:</p>
+                    <p className="text" >{coin.last_update}</p>
+                </div>
+            </div>
         </>
     )
 }
