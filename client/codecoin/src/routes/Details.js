7a07a2ef12561d2860689bc11b9a106b67371f0b
barramartin <78955308+barramartin@users.noreply.github.com>
2021-12-15 14:40:10 +0100

    feat[#34]: Details.js displays the proper header

--- client/codecoin/src/routes/Details.js
+++ client/codecoin/src/routes/Details.js
@@ -1,17 +1,19 @@
 import {useParams} from "react-router-dom";
 import CoinFetcher from "../components/CoinFetcher";
+import Button from "../components/Button";
+import {Link} from "react-router-dom";
+import CoinDetailsModule from "../styles/CoinDetails.css"
 
 function Details() {
     let { id } = useParams();
     const coin = CoinFetcher(id);
     return (
         <>
-            <div>
-                    <p>{coin.name}</p>
-                    <p>{coin.current_price}</p>
-                    <p>{coin.high_24h}</p>
-                    <p>{coin.low_24h}</p>
+            <div className={"header"}>
+                <img src={coin.image} className={"detailed-img"} alt=""/>
+                <h1>{coin.name}</h1>
             </div>
+
         </>
     )
 }
