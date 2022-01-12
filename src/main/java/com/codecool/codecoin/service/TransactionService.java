e00114f97fd5c51e42e6a2bf5c819c36318b90e4
szirakibalint <blntsziraki@gmail.com>
2022-01-12 13:53:31 +0100

    refactor[#64]: use portfolioService in TransactionService
    BREAKING CHANGE

--- src/main/java/com/codecool/codecoin/service/TransactionService.java
+++ src/main/java/com/codecool/codecoin/service/TransactionService.java
@@ -1,20 +1,21 @@
 package com.codecool.codecoin.service;
 
-import com.codecool.codecoin.model.*;
+import com.codecool.codecoin.model.Transaction;
+import com.codecool.codecoin.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.math.BigDecimal;
 
 import static com.codecool.codecoin.model.TransactionType.BUY;
 
 @Service
 public class TransactionService {
     private final UserService userService;
+    private final PortfolioService portfolioService;
 
     @Autowired
-    public TransactionService(UserService userService) {
+    public TransactionService(UserService userService, PortfolioService portfolioService) {
         this.userService = userService;
+        this.portfolioService = portfolioService;
     }
 
     public boolean handleTransaction(Transaction transaction) {
@@ -28,24 +29,19 @@ public class TransactionService {
     }
 
     private boolean initTransaction(User user, Transaction transaction) {
-        Portfolio portfolio = user.getPortfolio();
         if (transaction.getType() == BUY) {
-            return initBuy(portfolio, transaction);
+            return initBuy(user, transaction);
         } else {
-            return initSell(portfolio, transaction);
+            return initSell(user, transaction);
         }
     }
 
-    private boolean initBuy(Portfolio portfolio, Transaction transaction) {
-        Cryptocurrency crypto = transaction.getCrypto();
-        BigDecimal amount = transaction.getCryptoAmount();
-        return portfolio.sellCrypto(crypto, amount);
+    private boolean initBuy(User user, Transaction transaction) {
+        return portfolioService.buyCrypto(user, transaction);
     }
 
-    private boolean initSell(Portfolio portfolio, Transaction transaction) {
-        Cryptocurrency crypto = transaction.getCrypto();
-        BigDecimal amount = transaction.getCryptoAmount();
-        return portfolio.buyCrypto(crypto, amount);
+    private boolean initSell(User user, Transaction transaction) {
+        return portfolioService.sellCrypto(user, transaction);
     }
 
     private void recordTransaction(User user, Transaction transaction) {
