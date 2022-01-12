16bdf8b9f1946b388034fd65a9602289381bab72
szirakibalint <blntsziraki@gmail.com>
2022-01-12 10:41:51 +0100

    feat[#64]: add TransactionService class

---
+++ src/main/java/com/codecool/codecoin/service/TransactionService.java
@@ -0,0 +1,39 @@
+package com.codecool.codecoin.service;
+
+import com.codecool.codecoin.model.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+import static com.codecool.codecoin.model.TransactionType.BUY;
+
+@Service
+public class TransactionService {
+    private final UserService userService;
+
+    @Autowired
+    public TransactionService(UserService userService) {
+        this.userService = userService;
+    }
+
+    private void handleTransaction(Transaction transaction) {
+
+    }
+
+    private boolean initTransaction(User user, Transaction transaction) {
+        return false;
+    }
+
+    private boolean initBuy(Portfolio portfolio, Transaction transaction) {
+        return false;
+    }
+
+    private boolean initSell(Portfolio portfolio, Transaction transaction) {
+        return false;
+    }
+
+    private void recordTransaction(User user, Transaction transaction) {
+
+    }
+}
