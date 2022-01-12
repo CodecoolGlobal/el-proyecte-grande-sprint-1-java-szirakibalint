8f849ce9fae61c5f6f688ae643dce0148e955800
szirakibalint <blntsziraki@gmail.com>
2022-01-12 15:16:07 +0100

    fix[#64]: make requests in frontend work with new backend

--- client/codecoin/src/routes/Sell.js
+++ client/codecoin/src/routes/Sell.js
@@ -31,7 +31,7 @@ function Sell() {
                 </div>
             <div className={"secondary-container"} style={secondaryContainer}>
                 <div className="subheader">
-                    <h3>Balance: {portfolio.portfolio !== undefined && portfolio.portfolio.currencies !== undefined && portfolio.portfolio.currencies.USD || 0} USD</h3>
+                    <h3>Balance: {portfolio.portfolio !== undefined && portfolio.portfolio.currencies !== undefined && portfolio.currencyBalance || 0} USD</h3>
                     <h3>{coin.name} balance: {portfolio.portfolio !== undefined && portfolio.portfolio.cryptoCurrencies !== undefined && portfolio.portfolio.cryptoCurrencies[coin.id] || 0} {coin.symbol}</h3>
                     <h3>1 {coin.symbol} = {coin.current_price} USD</h3>
                 </div>
