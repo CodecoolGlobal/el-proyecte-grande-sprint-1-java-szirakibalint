a3e2984d77d1a8cd6041495652ff8a4a6e85b52c
szirakibalint <blntsziraki@gmail.com>
2022-01-12 10:22:04 +0100

    feat[#64]: implement newUserDAO methods in UserService

--- src/main/java/com/codecool/codecoin/service/UserService.java
+++ src/main/java/com/codecool/codecoin/service/UserService.java
@@ -1,7 +1,9 @@
 package com.codecool.codecoin.service;
 
+import com.codecool.codecoin.dao.NewUserDAO;
 import com.codecool.codecoin.dao.UserDAO;
 import com.codecool.codecoin.model.Portfolio;
+import com.codecool.codecoin.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,12 +13,22 @@ import java.math.BigDecimal;
 public class UserService {
 
     private final UserDAO userDAO;
+    private final NewUserDAO newUserDAO;
     private final Calculator calculator;
 
     @Autowired
-    public UserService(UserDAO userDAO, Calculator calculator) {
+    public UserService(UserDAO userDAO, NewUserDAO newUserDAO, Calculator calculator) {
         this.userDAO = userDAO;
+        this.newUserDAO = newUserDAO;
         this.calculator = calculator;
+    }
+
+    public User findById(Long id) {
+        return newUserDAO.findById(id);
+    }
+
+    public void save(User user) {
+        newUserDAO.save(user);
     }
 
     public Portfolio getPortfolio() {
