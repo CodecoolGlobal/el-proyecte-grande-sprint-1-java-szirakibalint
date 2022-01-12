5fc2faf0698f1e0a0960bfa699f4201f62c75c80
szirakibalint <blntsziraki@gmail.com>
2022-01-12 10:21:20 +0100

    feat[#64]: implement hashCode and equals in User class

--- src/main/java/com/codecool/codecoin/model/User.java
+++ src/main/java/com/codecool/codecoin/model/User.java
@@ -2,6 +2,7 @@ package com.codecool.codecoin.model;
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.Objects;
 
 public class User {
     private Long id;
@@ -11,4 +12,17 @@ public class User {
     private CurrencyType preferredCurrency;
     private BigDecimal currencyBalance;
     private List<Transaction> transactions;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id.equals(user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
