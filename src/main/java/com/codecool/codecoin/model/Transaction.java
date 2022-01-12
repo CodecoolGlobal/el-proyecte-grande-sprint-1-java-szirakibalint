5290b7750afd201986ee470f5d11f303ad778684
szirakibalint <blntsziraki@gmail.com>
2022-01-12 10:20:30 +0100

    feat[#64]: add TransactionType field to Transaction

--- src/main/java/com/codecool/codecoin/model/Transaction.java
+++ src/main/java/com/codecool/codecoin/model/Transaction.java
@@ -5,6 +5,7 @@ import java.math.BigDecimal;
 public class Transaction {
     private Long id;
     private Long userId;
+    private TransactionType type;
     private Cryptocurrency crypto;
     private BigDecimal cryptoAmount;
     private CurrencyType currency;
