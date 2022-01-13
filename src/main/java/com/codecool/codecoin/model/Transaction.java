799a138723fdc4b6bc2a2459e85c1f1b8860ead6
Balint Kozari <mrkoba@gmail.com>
2022-01-13 10:19:27 +0100

    feat[#60]: add entity annotation to transaction class

--- src/main/java/com/codecool/codecoin/model/Transaction.java
+++ src/main/java/com/codecool/codecoin/model/Transaction.java
@@ -3,18 +3,24 @@ package com.codecool.codecoin.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @Getter
 @Setter
+@Entity
 public class Transaction {
+    @Id
+    @GeneratedValue
     private Long id;
     private Long userId;
     private LocalDateTime date;
     private TransactionType type;
     private String cryptoId;
-    private Cryptocurrency crypto;
+    //private Cryptocurrency crypto;
     private BigDecimal cryptoAmount;
     private CurrencyType currency;
     private BigDecimal currencyAmount;
