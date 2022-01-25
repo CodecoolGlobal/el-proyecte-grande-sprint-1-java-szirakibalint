4bb7a2a7e70c42f7acf0205ceea91a6b81c00c91
Balint Kozari <mrkoba@gmail.com>
2022-01-25 15:03:28 +0100

    feat[#70]: create query for getting usernames

--- src/main/java/com/codecool/codecoin/repositories/UserRepository.java
+++ src/main/java/com/codecool/codecoin/repositories/UserRepository.java
@@ -2,6 +2,12 @@ package com.codecool.codecoin.repositories;
 
 import com.codecool.codecoin.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    @Query("SELECT user.username FROM RegularUser user")
+    List<String> findUsernames();
 }
