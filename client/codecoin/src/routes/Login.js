3de3d35b1fb0c719c3131dc51209bdb28908a590
Balint Kozari <mrkoba@gmail.com>
2022-01-25 11:28:26 +0100

    fix[#70]: replace for with htmlFor

--- client/codecoin/src/routes/Login.js
+++ client/codecoin/src/routes/Login.js
@@ -5,9 +5,9 @@ function Login() {
         <div className={"sign-content"}>
             <div className="sign-details">
                 <h1 className={"form-title"}>Sign in</h1>
-                <label for={"email"}>Email</label>
+                <label htmlFor={"email"}>Email</label>
                 <input className={"form-input"} id={"email"} name={"email"} type={"text"} placeholder={"code.coin@iotiger.hello"}/>
-                <label for={"password"}>Password</label>
+                <label htmlFor={"password"}>Password</label>
                 <input className={"form-input"} id={"confirm-password"} name={"password"} type={"password"} placeholder={"code.coin@iotiger.hello"}/>
                 <button className={"button purple-button"}>Sign in</button>
             </div>
