8f81532c9b99f4990d417652e0056fced5ece08c
Balint Kozari <mrkoba@gmail.com>
2022-01-26 10:12:24 +0100

    feat[#70]: store user data after registration

--- client/codecoin/src/routes/Register.js
+++ client/codecoin/src/routes/Register.js
@@ -4,11 +4,12 @@ import {useEffect, useState} from "react";
 
 function Register() {
 
-    //const usernames = useUsers('',true);
+    const usernames = useUsers('',true);
     const[username, setUsername] = useState([]);
     const[firstPassword, setFirstPassword] = useState([]);
     const[secondPassword, setSecondPassword] = useState([]);
     const[buttonDisabled, setButtonDisabled] = useState(true);
+    const[errors, setErrors] = useState("");
 
     useEffect(() => {
         setButtonDisabled(!validateData());
@@ -19,8 +20,42 @@ function Register() {
     }
 
     function validateData() {
-        return !(firstPassword !== secondPassword || firstPassword.length < 6 || username.length < 6);
+        let errorMessages = "";
+        if (firstPassword !== secondPassword) {
+            errorMessages += "Passwords does not match\n";
+        }
+        if (firstPassword.length < 6) {
+            errorMessages += "Password is too short\n";
+        }
+        if (username.length < 5 || !username.includes("@") || !username.includes(".")) {
+            errorMessages += "Email is invalid\n";
+        }
+        for (const username1 of usernames) {
+            if (username1 === username) {
+                errorMessages += "This email is already taken\n";
+                break;
+            }
+        }
+        setErrors(errorMessages);
+        return errorMessages === "";
     }
+
+    async function submitRegistration() {
+        if (validateData()) {
+            await fetch(`/api/users`, {
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                method: 'POST',
+                body: JSON.stringify({
+                    "username": username,
+                    "password": firstPassword
+                })
+            })
+        }
+    }
+
     return (
         <div>
             <div className={"sign-content"}>
@@ -32,7 +67,8 @@ function Register() {
                     <input className={"form-input"} id={"password"} name={"password"} type={"password"} onChange={event => updateInputValue(event, setFirstPassword)}/>
                     <label htmlFor={"confirm-password"}>Confirm password</label>
                     <input className={"form-input"} id={"confirm-password"} name={"confirm-password"} type={"password"} onChange={event => updateInputValue(event, setSecondPassword)} />
-                    <button className={"button purple-button"} disabled={buttonDisabled}>Sign in</button>
+                    <button className={"button purple-button"} disabled={buttonDisabled} onClick={submitRegistration}>Register</button>
+                    <div>{errors}</div>
                 </div>
             </div>
         </div>
@@ -42,7 +78,4 @@ function Register() {
 export default Register;
 
 
-//email contains @ and .
 //password contains one capital letter, number, and longer or equal than 8 chars
-// confirmed pass is equals password
-//one thingie, backend could check if it is already exists or not.
