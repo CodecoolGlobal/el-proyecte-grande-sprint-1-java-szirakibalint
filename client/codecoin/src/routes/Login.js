c85ed0dd8c0e67f722cfe2f1ab3b64ba476185fb
barramartin <78955308+barramartin@users.noreply.github.com>
2022-01-26 11:23:03 +0100

    feat[#70]: make Login.js submit request for logging in

--- client/codecoin/src/routes/Login.js
+++ client/codecoin/src/routes/Login.js
@@ -1,21 +1,51 @@
 import LoginModule from "../styles/Login.css"
+import {useState, useContext} from "react";
 
 function Login() {
+    const[username, setUsername] = useState([]);
+    const[password, setPassword] = useState([]);
+    const[errorMessage, setErrorMessage] = useState("");
+
+    function updateInputValue(event, setInput) {
+        setInput(event.target.value);
+    }
+
+    async function submitLogin() {
+        const response = await fetch(`/api/users/login`, {
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            method: 'POST',
+            body: JSON.stringify({
+                "username": username,
+                "password": password
+            })
+        })
+        const {id} = await response.json();
+        if (id === "-1") {
+            setErrorMessage("Invalid email or password");
+        }
+        else {
+            sessionStorage.setItem("user-id", id);
+            window.location.href = `/portfolio/${id}`;
+        }
+
+    }
+
     return (
         <div className={"sign-content"}>
             <div className="sign-details">
                 <h1 className={"form-title"}>Sign in</h1>
                 <label htmlFor={"email"}>Email</label>
-                <input className={"form-input"} id={"email"} name={"email"} type={"text"} placeholder={"code.coin@iotiger.hello"}/>
+                <input className={"form-input"} id={"email"} name={"email"} type={"text"} placeholder={"code.coin@iotiger.hello"} onChange={event => updateInputValue(event, setUsername)}/>
                 <label htmlFor={"password"}>Password</label>
-                <input className={"form-input"} id={"confirm-password"} name={"password"} type={"password"} placeholder={"code.coin@iotiger.hello"}/>
-                <button className={"button purple-button"}>Sign in</button>
+                <input className={"form-input"} id={"confirm-password"} name={"password"} type={"password"} placeholder={"code.coin@iotiger.hello"} onChange={event => updateInputValue(event, setPassword)}/>
+                <button onClick={submitLogin} className={"button purple-button"}>Sign in</button>
+                <div>{errorMessage}</div>
             </div>
         </div>
     )
 }
-
-//maybe email validation: required, @, one .
-//password validation necessary?
 
 export default Login;
