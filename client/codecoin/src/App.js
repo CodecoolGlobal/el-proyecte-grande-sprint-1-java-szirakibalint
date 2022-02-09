33188f1114d69347a0b708ea327716c1b93e4c15
barramartin <78955308+barramartin@users.noreply.github.com>
2022-02-09 10:58:03 +0100

    feat[#70]: create context provider for UserContext in App.js

--- client/codecoin/src/App.js
+++ client/codecoin/src/App.js
@@ -9,12 +9,25 @@ import Portfolio from "./routes/Portfolio";
 import Navbar from "./components/Navbar";
 import Register from "./routes/Register";
 import Login from "./routes/Login";
+import {useEffect, useState} from "react";
+import {UserContext} from "./components/UserContext"
 
 
 function App() {
+    let [isLoggedIn, setIsLoggedIn] = useState(null);
 
-  return (
+    useEffect(() => {
+        setIsLoggedIn(userLoggedIn());
+    }, [])
+
+    function userLoggedIn() {
+        return sessionStorage?.getItem("user-id") !== null;
+    }
+
+
+    return (
       <div className="App">
+          <UserContext.Provider value={[isLoggedIn, setIsLoggedIn]}>
       <Navbar/>
       <Routes>
           <Route path="/" exact element={<LandingPage/>}/>
@@ -24,9 +37,10 @@ function App() {
           <Route path="/coins/:id/sell" element={<Sell/>}/>
           <Route path="/portfolio" element={<Portfolio/>}/>
           <Route path={"/register"} element={<Register/>}/>
-          <Route path={"/login"} element={<Login/>}/>
+          <Route path={"/login"} element={<Login />}/>
           <Route path="*" element={<Error/>}/>
       </Routes>
+          </UserContext.Provider>
       </div>
   );
 }
