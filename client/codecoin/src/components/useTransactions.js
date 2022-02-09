e94f1017dd4f0b35ebac28d5cf2b6de3a409621b
szirakibalint <blntsziraki@gmail.com>
2022-02-09 14:53:31 +0100

    feat[#72]: add useTransactions custom hook

---
+++ client/codecoin/src/components/useTransactions.js
@@ -0,0 +1,32 @@
+import React, {useEffect, useState} from 'react';
+import Error from "../routes/Error";
+
+function useTransactions() {
+    const [transactions, setTransactions] = useState([])
+    useEffect(async () => {
+        const transactionsFromAPI = await fetchTransactions()
+        setTransactions(transactionsFromAPI)
+    }, []);
+    const userId = sessionStorage.getItem("user-id")
+
+    const fetchTransactions = async () => {
+        try {
+            const jwt = sessionStorage.getItem("jwt");
+            const res = await fetch(
+                `/api/transactions/${userId}`,
+                {
+                    headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${jwt}`
+                    }
+                })
+            return await res.json()
+        } catch (e) {
+            return Error(`Error: failed to fetch transaction history from API endpoint /api/transactions/${userId}`);
+        }
+    }
+    return transactions;
+}
+
+export default useTransactions;
