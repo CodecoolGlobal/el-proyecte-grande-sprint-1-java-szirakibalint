bb87e26c58e6f1b4805d28d78189f90abf20327b
barramartin <78955308+barramartin@users.noreply.github.com>
2022-02-09 10:59:41 +0100

    feat[#70]: make Navbar.js change depending on whether the user is logged in or not

--- client/codecoin/src/components/Navbar.js
+++ client/codecoin/src/components/Navbar.js
@@ -1,7 +1,29 @@
 import {Link} from "react-router-dom";
 import NavbarModule from "../styles/Navbar.css"
+import {useContext, useEffect} from "react";
+import {UserContext} from "./UserContext";
 
 function Navbar() {
+    let [isLoggedIn, setIsLoggedIn] = useContext(UserContext);
+    function clearSession() {
+        sessionStorage.clear();
+        setIsLoggedIn(false);
+    }
+    if (isLoggedIn) {
+        return (
+            <div className="navbar">
+                <div id="logo-container">
+                    <Link to='/'><img src='https://i.postimg.cc/J46SZv3g/logo.png' alt='logo'/></Link>
+                </div>
+                <div id="nav-options">
+                    <Link to="/coins">Currencies</Link>
+                    <Link to="/portfolio">Portfolio</Link>
+                    <Link onClick={clearSession} to="/coins">Logout</Link>
+                </div>
+            </div>
+        )
+    }
+    else {
     return (
         <div className="navbar">
             <div id="logo-container">
@@ -9,12 +31,12 @@ function Navbar() {
             </div>
             <div id="nav-options">
                 <Link to="/coins">Currencies</Link>
-                <Link to="/portfolio">Portfolio</Link>
                 <Link to="/register">Register</Link>
                 <Link to="/login">Login</Link>
             </div>
         </div>
     )
+    }
 }
 
 export default Navbar;
